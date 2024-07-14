a0a3bf85cf565c6bd1b9ba67b8df327290964712
Josquin2 <Josquinwork@mail.ru>
2024-07-14 19:36:44 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inventory-app black">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+.inventory-app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #1d1d1d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main-view">
+    <div class="center-view">
+      <div class="item-info"></div>
+      <div class="main-inventory"></div>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.main-view {
+  width: 849px;
+  height: 660px;
+  border: 1px solid #4d4d4d;
+  border-radius: 10px;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  .center-view {
+    display: flex;
+    gap: 24px;
+
+    .item-info {
+      width: 236px;
+      height: 500px;
+      border: 1px solid #4d4d4d;
+      border-radius: 12px;
+      padding: 18px 14px;
+      background-color: #262626;
+    }
+
+    .main-inventory {
+      width: 525px;
+      height: 500px;
+      border: 1px solid #3c3c43;
+      border-radius: 12px;
+      background-color: #262626;
+    }
+  }
+
+  .footer {
+    width: 785px;
+    height: 72px;
+    border: 1px solid #3c3c43;
+    border-radius: 12px;
+    background-color: #262626;
+  }
+}
+</style>
